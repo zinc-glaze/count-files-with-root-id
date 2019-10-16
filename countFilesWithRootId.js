c83d2b6693a3862d5a000ac28257f6e917a17883
Zinc Glaze <graves.ezekiel@gmail.com>
2019-10-16 11:56:43 -0400

    cleaned up comments and code

--- countFilesWithRootId.js
+++ countFilesWithRootId.js
@@ -12,25 +12,23 @@ let rootIds = fs.readFileSync(`${__dirname}/root_ids.txt`, 'utf8').trim().split(
 //load file ids into array
 let fileIds = fs.readFileSync(`${__dirname}/file_ids.txt`, 'utf8').trim().split(',');
 
-// console.log(rootIds.length);
-// console.log(fileIds.length);
+console.log('Total root IDs:', rootIds.length);
+console.log('Total file IDs:', fileIds.length);
 
 //iterate through root ids
 for (let i in rootIds) {
     let fileCounter = 0;
     let currentRootId = rootIds[i];
-    //console.log(currentRootId);
     //iterate through file ids and count instances of root id
     for (let i in fileIds) {
         if (fileIds[i].includes(currentRootId)){
             fileCounter += 1;
         }
     }
-    //console.log(fileCounter);
     try {
         fs.appendFileSync('file_count.txt', `${fileCounter}\r\n`);
     } catch (e) {
         console.log("Cannot write file ", e);
     }
 }
-console.log('Finished!');
+console.log('Finished! Check generated file \'file_count.txt\' for counts.');
